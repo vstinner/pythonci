d3a27541c146cb2f603323e50bb9778943f0f203
Victor Stinner <vstinner@python.org>
2019-10-08 17:42:43 +0200

    Update numpy to 1.17.2

--- pythonci/task/numpy.py
+++ pythonci/task/numpy.py
@@ -2,7 +2,7 @@ import os.path
 from pythonci.task import BaseTask
 
 
-NUMPY_ZIP = 'https://files.pythonhosted.org/packages/da/32/1b8f2bb5fb50e4db68543eb85ce37b9fa6660cd05b58bddfafafa7ed62da/numpy-1.17.0.zip'
+NUMPY_ZIP = 'https://files.pythonhosted.org/packages/ac/36/325b27ef698684c38b1fe2e546e2e7ef9cecd7037bcdb35c87efec4356af/numpy-1.17.2.zip'
 
 
 class Task(BaseTask):
