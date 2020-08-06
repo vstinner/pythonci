1cf085e880a86dc5ee16c803d667391c56103bdd
Victor Stinner <vstinner@python.org>
2020-08-06 15:39:17 +0200

    update jinja

--- pythonci/task/jinja.py
+++ pythonci/task/jinja.py
@@ -1,7 +1,7 @@
 from pythonci.task import BaseTask
 
 
-JINJA_TARBALL = 'https://files.pythonhosted.org/packages/d8/03/e491f423379ea14bb3a02a5238507f7d446de639b623187bccc111fbecdf/Jinja2-2.11.1.tar.gz'
+JINJA_TARBALL = 'https://files.pythonhosted.org/packages/64/a7/45e11eebf2f15bf987c3bc11d37dcc838d9dc81250e67e4c5968f6008b6c/Jinja2-2.11.2.tar.gz'
 
 
 
